b5b3a4558d6e5f21c6b344047b73a145877a63f1
wyaxinw <a114221084@mail.shu.edu.tw>
2025-10-08 11:41:47 +0800

    114_10_08

---
+++ 114_10_08/Tutorial 3-3/Sale Price Calculator/Sale Price Calculator/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sale_Price_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            decimal originaPrice;
+            decimal discountPercentage;
+            decimal salePrice;
+
+            originaPrice = decimal.Parse(originalPriceTextBox.Text);
+            discountPercentage = decimal.Parse(discountPercentageTextBox.Text);
+
+
+            salePrice = originaPrice * (1 - discountPercentage / 100.0m);
+
+
+            salePriceLabel.Text = salePrice.ToString("C");
+            
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+
+        private void originalPriceTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
