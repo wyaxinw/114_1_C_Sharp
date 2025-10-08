460dc1b761e27db720f14b84c72d18c5064192a5
wyaxinw <a114221084@mail.shu.edu.tw>
2025-10-08 12:56:56 +0800

    114_10_08

---
+++ 114_10_08/Tutorial 3-4/Test Average/Test Average/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Test_Average
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.test1TextBox = new System.Windows.Forms.TextBox();
+            this.test2TextBox = new System.Windows.Forms.TextBox();
+            this.test3TextBox = new System.Windows.Forms.TextBox();
+            this.test1PromptLabel = new System.Windows.Forms.Label();
+            this.test2PromptLabel = new System.Windows.Forms.Label();
+            this.test3PromptLabel = new System.Windows.Forms.Label();
+            this.averageLabel = new System.Windows.Forms.Label();
+            this.outputDescriptionLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // test1TextBox
+            // 
+            this.test1TextBox.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.test1TextBox.Location = new System.Drawing.Point(275, 24);
+            this.test1TextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.test1TextBox.Name = "test1TextBox";
+            this.test1TextBox.Size = new System.Drawing.Size(366, 66);
+            this.test1TextBox.TabIndex = 0;
+            // 
+            // test2TextBox
+            // 
+            this.test2TextBox.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.test2TextBox.Location = new System.Drawing.Point(275, 110);
+            this.test2TextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.test2TextBox.Name = "test2TextBox";
+            this.test2TextBox.Size = new System.Drawing.Size(366, 66);
+            this.test2TextBox.TabIndex = 1;
+            // 
+            // test3TextBox
+            // 
+            this.test3TextBox.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.test3TextBox.Location = new System.Drawing.Point(275, 194);
+            this.test3TextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.test3TextBox.Name = "test3TextBox";
+            this.test3TextBox.Size = new System.Drawing.Size(366, 66);
+            this.test3TextBox.TabIndex = 2;
+            // 
+            // test1PromptLabel
+            // 
+            this.test1PromptLabel.AutoSize = true;
+            this.test1PromptLabel.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.test1PromptLabel.Location = new System.Drawing.Point(82, 35);
+            this.test1PromptLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.test1PromptLabel.Name = "test1PromptLabel";
+            this.test1PromptLabel.Size = new System.Drawing.Size(166, 55);
+            this.test1PromptLabel.TabIndex = 3;
+            this.test1PromptLabel.Text = "測驗一:";
+            // 
+            // test2PromptLabel
+            // 
+            this.test2PromptLabel.AutoSize = true;
+            this.test2PromptLabel.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.test2PromptLabel.Location = new System.Drawing.Point(81, 121);
+            this.test2PromptLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.test2PromptLabel.Name = "test2PromptLabel";
+            this.test2PromptLabel.Size = new System.Drawing.Size(166, 55);
+            this.test2PromptLabel.TabIndex = 4;
+            this.test2PromptLabel.Text = "測驗二:";
+            this.test2PromptLabel.Click += new System.EventHandler(this.test2PromptLabel_Click);
+            // 
+            // test3PromptLabel
+            // 
+            this.test3PromptLabel.AutoSize = true;
+            this.test3PromptLabel.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.test3PromptLabel.Location = new System.Drawing.Point(82, 205);
+            this.test3PromptLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.test3PromptLabel.Name = "test3PromptLabel";
+            this.test3PromptLabel.Size = new System.Drawing.Size(166, 55);
+            this.test3PromptLabel.TabIndex = 5;
+            this.test3PromptLabel.Text = "測驗三:";
+            // 
+            // averageLabel
+            // 
+            this.averageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.averageLabel.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.averageLabel.Location = new System.Drawing.Point(275, 290);
+            this.averageLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.averageLabel.Name = "averageLabel";
+            this.averageLabel.Size = new System.Drawing.Size(390, 55);
+            this.averageLabel.TabIndex = 6;
+            this.averageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.averageLabel.Click += new System.EventHandler(this.averageLabel_Click);
+            // 
+            // outputDescriptionLabel
+            // 
+            this.outputDescriptionLabel.AutoSize = true;
+            this.outputDescriptionLabel.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.outputDescriptionLabel.Location = new System.Drawing.Point(38, 290);
+            this.outputDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.outputDescriptionLabel.Name = "outputDescriptionLabel";
+            this.outputDescriptionLabel.Size = new System.Drawing.Size(210, 55);
+            this.outputDescriptionLabel.TabIndex = 7;
+            this.outputDescriptionLabel.Text = "平均分數:";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.calculateButton.Location = new System.Drawing.Point(13, 363);
+            this.calculateButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(250, 69);
+            this.calculateButton.TabIndex = 8;
+            this.calculateButton.Text = "計算平均";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.clearButton.Location = new System.Drawing.Point(315, 363);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(199, 69);
+            this.clearButton.TabIndex = 9;
+            this.clearButton.Text = "清除";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.exitButton.Location = new System.Drawing.Point(570, 367);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(156, 65);
+            this.exitButton.TabIndex = 10;
+            this.exitButton.Text = "離開";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 445);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.outputDescriptionLabel);
+            this.Controls.Add(this.averageLabel);
+            this.Controls.Add(this.test3PromptLabel);
+            this.Controls.Add(this.test2PromptLabel);
+            this.Controls.Add(this.test1PromptLabel);
+            this.Controls.Add(this.test3TextBox);
+            this.Controls.Add(this.test2TextBox);
+            this.Controls.Add(this.test1TextBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "測驗平均計算";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox test1TextBox;
+        private System.Windows.Forms.TextBox test2TextBox;
+        private System.Windows.Forms.TextBox test3TextBox;
+        private System.Windows.Forms.Label test1PromptLabel;
+        private System.Windows.Forms.Label test2PromptLabel;
+        private System.Windows.Forms.Label test3PromptLabel;
+        private System.Windows.Forms.Label averageLabel;
+        private System.Windows.Forms.Label outputDescriptionLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ 114_10_08/Tutorial 3-4/Test Average/Test Average/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 當使用者點擊「計算平均分數」按鈕時執行此事件處理函式
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            // 宣告三個分數變數來儲存三個測驗分數
+            double testl, test2, test3;
+          
+            // 嘗試將使用者輸入的文字轉換為數字
+            testl = double.Parse(test1TextBox.Text);
+            test2 = double.Parse(test2TextBox.Text);
+            test3 = double.Parse(test3TextBox.Text);
+            
+           // 計算三個分數的平均值
+          double average = (testl+ test2+ test3) / 3.0;
+
+            // 將平均分數格式化為兩位小數並顯示在averageLabel上
+            averageLabel.Text = average.ToString("n2");
+            }
+
+
+        // 當使用者點擊「清除」按鈕時執行此事件處理函式
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // 清除三個分數輸入框與平均分數顯示欄位
+            test1TextBox.Text = "";
+            test2TextBox.Text = "";
+            test3TextBox.Text = "";
+            // 清除平均分數顯示標籤的內容
+            averageLabel.Text = "";
+            // 將游標焦點移回第一個分數輸入框，方便使用者重新輸入
+            test1TextBox.Focus();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單
+            this.Close();
+        }
+
+        private void test2PromptLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void averageLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
